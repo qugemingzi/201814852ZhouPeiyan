102c2391bcecc4a0cc8660002b045c01e7286fb4
qugemingzi <1007950881@qq.com>
2018-11-13 20:20:46 +0800

    Homework 2:Naive Bayes

---
+++ Homework2/test.py
@@ -0,0 +1,14 @@
+# encoding=utf-8
+from collections import Counter
+
+x = {'apple': 1, 'banana': 2}
+y = {'banana': 10, 'pear': 11}
+print(sum(x.values()))
+z = {}
+z["a"] = Counter(x)
+z["b"] = Counter(y)
+print(z)
+z["c"] = z["a"] + z["b"]
+print(sum(z["c"].values()))
+print(z)
+print(2 ** 5)
